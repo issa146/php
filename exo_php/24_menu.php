4a756285eb6116c8f05d8e6883644c5a74c16f6f
issa146 <issaissaadi95@gmail.com>
2025-11-03 22:48:18 +0100

    j'ai fini

--- exo_php/24_menu.php
+++ exo_php/24_menu.php
@@ -33,22 +33,73 @@ $personnes = ["Issa","Hayata", "Ryan"];
 
 
 
+foreach ($personnes as $personne) {
+    echo "<h3>Menu pour $personne :</h3>";
 
-echo "Menu pour issa : " ."<br>". $menu["entrées"]["nom"][array_rand($menu["entrées"]["nom"])] ." ";
+     $total = 0;
 
-echo  $menu ['entrées'] ['prix']  [array_rand($menu["entrées"]["prix"])];
-echo " € ";
-echo "<br>";
+    foreach ($menu as $categorie => $items) {
+
+        $index = array_rand($items['nom']);
+
+        $nom = $items['nom'][$index];
+         $prix = $items['prix'][$index]; 
+
+         echo $categorie . " : " . $nom . " - " . $prix . " €<br>";
+
+         $total += $prix;
+        }
+
+            if ($total > 20) {
+            $reduction = $total * 0.10;
+            $total -= $reduction;
+            echo "Réduction de 10% appliquée :" . $reduction . " €<br>";
+        }
+        echo "Total à payer : " . $total . " €<br>";
+    }
+
+
+
+// foreach ($menu as $categorie => $items) {
+//     $indexNom = array_rand($items['nom']);
+//     $indexPrix = array_rand($items['prix']);
+
+//     echo  "Menu pour issa : " ."<br>". $categorie . " : " . $items['nom'][$indexNom] . " - " . $items['prix'][$indexPrix] . " €<br>";
+// }
+
+// foreach ($menu as $categorie => $items) {
+//     $indexNom = array_rand($items['nom']);
+//     $indexPrix = array_rand($items['prix']);
+
+//     echo  "Menu pour hayata : " ."<br>". $categorie . " : " . $items['nom'][$indexNom] . " - " . $items['prix'][$indexPrix] . " €<br>";
+// }
+
+// foreach ($menu as $categorie => $items) {
+//     $indexNom = array_rand($items['nom']);
+//     $indexPrix = array_rand($items['prix']);
+
+//     echo  "Menu pour ryan : " ."<br>". $categorie . " : " . $items['nom'][$indexNom] . " - " . $items['prix'][$indexPrix] . " €<br>";
+// }
+
+
+
+// echo "Menu pour issa : " ."<br>". $menu["entrées"]["nom"][array_rand($menu["entrées"]["nom"])] ." ";
+
+// echo  $menu ['entrées'] ['prix']  [array_rand($menu["entrées"]["prix"])];
+// echo " € ";
+// echo "<br>";
 // -------------------------------------------------------------------------------------------------------------
-echo $menu["plats"]["nom"][array_rand($menu["plats"]["nom"])]." ";
-echo $menu ['plats'] ['prix'] [array_rand($menu["plats"]["prix"])];
-echo " € ";
-echo "<br>";
+// echo $menu["plats"]["nom"][array_rand($menu["plats"]["nom"])]." ";
+// echo $menu ['plats'] ['prix'] [array_rand($menu["plats"]["prix"])];
+// echo " € ";
+// echo "<br>";
 // --------------------------------------------------------------------------------------------------------
-echo $menu["dessert"]["nom"][array_rand($menu["dessert"]["nom"])]." ";
-echo $menu ['dessert'] ['prix'] [array_rand($menu["dessert"]["prix"])];
-echo " € ";
-echo "<br>";
+// echo $menu["dessert"]["nom"][array_rand($menu["dessert"]["nom"])]." ";
+// echo $menu ['dessert'] ['prix'] [array_rand($menu["dessert"]["prix"])];
+// echo " € ";
+// echo "<br>";
+
+
 
 
 
